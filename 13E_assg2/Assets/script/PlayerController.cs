55b2d5c15243f1ad942fb0910ebf436ff59e8175
ChngYaXuanRenee <116330291+ChngYaXuanRenee@users.noreply.github.com>
2023-06-18 23:00:20 +0800

    adding camera movement properties changes

--- 13E_assg2/Assets/script/PlayerController.cs
+++ 13E_assg2/Assets/script/PlayerController.cs
@@ -1,4 +1,3 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,8 +6,8 @@ using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    Vector3 rotationInput = Vector3.zero;
-    Vector3 moveData = Vector3.zero;
+    Vector2 rotationInput = Vector2.zero;
+    Vector2 moveData = Vector2.zero;
 
     float rotationSpeed = 1f;
     float moveSpeed = 0.19f;
@@ -21,16 +20,16 @@ public class PlayerController : MonoBehaviour
 
     //private void OnCollisionEnter(Collision collision)
     //{
-        //if (collision.gameObject.tag == "collectables")
-        //{
-            //Debug.Log("Enter : " + collision.gameObject.name);
-            //Destroy(collision.gameObject);
-        //}
+    //if (collision.gameObject.tag == "collectables")
+    //{
+    //Debug.Log("Enter : " + collision.gameObject.name);
+    //Destroy(collision.gameObject);
+    //}
     //}
 
     void OnLook(InputValue value)
     {
-        rotationInput.y = value.Get<Vector2>().x;
+        rotationInput = value.Get<Vector2>();
     }
 
     void OnMove(InputValue value)
@@ -41,12 +40,8 @@ public class PlayerController : MonoBehaviour
 
     }
 
-
     public AudioSource bgm;
     public bool playing = false;
-
-
-
 
     void onFire()
     {
@@ -57,41 +52,27 @@ public class PlayerController : MonoBehaviour
         transform.position += new Vector3(2f, 0, 0);
     }
 
-
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0, rotationInput.y) * rotationSpeed);
-        head.rotation = Quaternion.Euler(head.rotation.eulerAngles + new Vector3(rotationInput.x, 0) * rotationSpeed);
-        //headTransform.rotation = Quaternion.Euler(headTransform.rotation.eulerAngles + new Vector3(rotationInput.x, 0) * rotationSpeed);
-        //headGameObject.transform.rotation = Quaternion.Euler(headGameObject.transform.rotation.eulerAngles + new Vector3(rotationInput.x, 0) * rotationSpeed);
-        // transform.rotation =
-        //  Quaternion.Euler(transform.rotation.eulerAngles + rotationInput * rotationSpeed);
-
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0, rotationInput.x) * rotationSpeed);
+        head.rotation = Quaternion.Euler(head.rotation.eulerAngles + new Vector3(-rotationInput.y, 0) * rotationSpeed);
 
         Vector3 forwardDir = transform.forward;
         Vector3 rightDir = transform.right;
 
         var moveForward = forwardDir * moveData.y;
-        var moveRight = forwardDir * moveData.x;
+        var moveRight = rightDir * moveData.x;
 
-        GetComponent<Rigidbody>().MovePosition(transform.position
-            + (moveForward + moveRight) * moveSpeed);
-
-
-
-        transform.position +=
-       (forwardDir * moveData.y
-        + rightDir * moveData.x)
-        * moveSpeed;
-
+        GetComponent<Rigidbody>().MovePosition(transform.position + (moveForward + moveRight) * moveSpeed);
     }
+
     void OnJump()
     {
         //jump 
