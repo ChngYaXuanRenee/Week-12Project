8cc263b543271068e2687858913ce30ee393fb85
ChngYaXuanRenee <116330291+ChngYaXuanRenee@users.noreply.github.com>
2023-06-27 18:41:34 +0800

    raycasted sound implementation

--- 13E_assg2/Assets/ObjectDestroyer.cs
+++ 13E_assg2/Assets/ObjectDestroyer.cs
@@ -4,29 +4,44 @@ using UnityEngine;
 
 public class ObjectDestroyer : MonoBehaviour
 {
-    //public string objectTagToDestory = "Destroyable";
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonUp (0))
-        {
-            Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 100f));
-            Vector3 direction = worldMousePosition - Camera.main.transform.position;
+    public AudioClip destroySoundClip; // Sound clip to play when an object is destroyed
+    private AudioSource audioSource;
 
+    private void Start()
+    {
+        // Create an empty game object as a child of the main camera
+        GameObject audioObject = new GameObject("DestroyAudio");
+        audioObject.transform.parent = Camera.main.transform;
+
+        // Add an AudioSource component to the audio object
+        audioSource = audioObject.AddComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            if (Physics.Raycast (Camera.main.transform.position, direction, out hit, 100f))
+            if (Physics.Raycast(ray, out hit))
             {
-                Debug.DrawLine(Camera.main.transform.position, hit.point, Color.green, 0.5f);
+                Debug.DrawLine(ray.origin, hit.point, Color.green, 0.5f);
+
                 if (hit.collider.CompareTag("Destroyable"))
                 {
+                    // Play the destroy sound clip
+                    if (destroySoundClip != null)
+                    {
+                        audioSource.PlayOneShot(destroySoundClip);
+                    }
+
                     Destroy(hit.collider.gameObject);
                 }
-                
             }
             else
             {
-                Debug.DrawLine(Camera.main.transform.position, worldMousePosition, Color.red, 0.5f);
+                Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100f, Color.red, 0.5f);
             }
         }
     }
