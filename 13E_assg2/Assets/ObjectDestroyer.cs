d48ae933b68b340dde0bf615f5bb36f359ebdb35
ChngYaXuanRenee <116330291+ChngYaXuanRenee@users.noreply.github.com>
2023-06-27 18:24:10 +0800

    Raycast action to destroy object

---
+++ 13E_assg2/Assets/ObjectDestroyer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDestroyer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp (0))
+        {
+            Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 100f));
+            Vector3 direction = worldMousePosition - Camera.main.transform.position;
+
+            RaycastHit hit;
+
+            if (Physics.Raycast (Camera.main.transform.position, direction, out hit, 100f))
+            {
+                Debug.DrawLine(Camera.main.transform.position, hit.point, Color.green, 0.5f);
+                Destroy(hit.collider.gameObject);
+            }
+            else
+            {
+                Debug.DrawLine(Camera.main.transform.position, worldMousePosition, Color.red, 0.5f);
+            }
+        }
+    }
+}
